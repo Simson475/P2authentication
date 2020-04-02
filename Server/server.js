86fbe87610c5114524a44a359a8074180155b52f
Jakob F Gregersen <lilleper.jfg@gmail.com>
2020-04-02 10:51:58 +0200

    Update server.js
    Den kan nu oprette en ny bruger i databasen og lave en dedikeret database til brugens passwords til specifikke sites.

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,22 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http'); //Giver os de nødvendige objekter 'req' og 'res' samt alt create server relateret
+const fs = require('fs'); //Giver os filfunktioner så vi kan skrive til og læse fra databasen
 
 
 http.createServer(async function(req, res) {
     console.log(" request was made: " + req.url + " with method " + req.method);
     switch (req.url) {
         case "/validate":
-            validator(req, res);
+            validator(req, res); //funktionskald af validator hvis url'en ender i /validate
             break;
-
+        case "/newUser":
+            masterAccount(req, res); //funktionskald der opretter bruger til password manageren
+            break;
+        case "/addAccount": //TODO tilføj funktion når html fil er klar.
+            res.writeHead(404, { "Content-Type": "text/plain" }); //Fejlmelding når URL'en ikke er genkendt
+            res.end("404, Site not found");
+            break;
         default:
-            res.writeHead(404, { "Content-Type": "text/plain" });
+            res.writeHead(404, { "Content-Type": "text/plain" }); //Fejlmelding når URL'en ikke er genkendt
             res.end("404, Site not found");
             break;
     }
@@ -26,13 +32,11 @@ console.log("listening at 3000");
  */
 async function validator(req, res) {
     res.writeHead(201, { "Content-Type": "application/json" })
-    let data = ""
-    let userData = null
-    await req.on("error", (err) => console.error(err))
-        .on("data", (chunk) => data += chunk)
-    data = JSON.parse(data)
+
+    let userData = null;
+    let data = await loadData(req);
     let database = JSON.parse(fs.readFileSync(__dirname + "/database.json"));
-    for (element of database) {
+    for (let element of database) {
         if (data.username == element.username && data.password == element.hashValue) {
             userData = JSON.parse(fs.readFileSync(__dirname + "/" + data.username + ".json"));
         }
@@ -40,4 +44,42 @@ async function validator(req, res) {
 
     console.log(userData);
     res.end(JSON.stringify(userData));
+}
+/**
+ * Creates an account for a user in the Password Manager
+ * @param {object} req 
+ * @param {object} res 
+ */
+async function masterAccount(req, res) {
+    res.writeHead(201, { "Content-type": "application/json" }) //TODO FIND UD AF FORMAT DER SENDES OG RET CONTENT TYPE Skriver header på res. til brugeren 
+    let data = await loadData(req); //Vi loader Fatimas funktion. loader dataen, skal ventes på.
+    let database = JSON.parse(fs.readFileSync(__dirname + "/database.json"));
+
+    for (let element of database) { //Denne iterative kontrolstruktur tjekker om brugernavnet er taget.
+        if (data.username == element.username) {
+            res.end(JSON.stringify("username is already taken")); //TODO indsæt token til 'Unavailable username'
+            return; //Hvis username er taget er der ingen grund til at iterere videre   
+        }
+    }
+
+    database.push({ username: data.username, hashValue: data.password }); //Vi pusher hele elementet til slutningen af array i database.
+    fs.writeFile("database.json", JSON.stringify(database, null, 2), function(err, data) { //TODO fund ud af format for error handling her og lav en if statement
+        /* STUB */
+    });
+
+    fs.writeFile(data.username + ".json", JSON.stringify(new Array(0), null, 2), function(err, data) { //TODO fund ud af format for error handling her og lav en if statement //Opretter en dedikeret fil der skal indeholde fremtidige sites med passwords.
+        res.end(JSON.stringify("user created"));
+    });
+}
+
+/**
+ * Loads data in bits and returns parsed to JSON.
+ * @param {object} req 
+ */
+async function loadData(req) {
+    let data = "";
+    await req.on("error", (err) => console.error(err)) //Denne funktion håndterer fejl og logge dem til konsollen
+        .on("data", (chunk) => data += chunk); //Når request modtager data skal den concatenate databrudstykkerne til ét stykke
+    data = JSON.parse(data); //Denne skal fortolke data til noget forståeligt JSON information.
+    return data;
 }
