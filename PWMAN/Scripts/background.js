3319a6e00b453049adf6aaebed62886cf80ce9f5
Simon Nielsen <Simonhelmingnielsen@gmail.com>
2020-04-14 20:17:19 +0200

    saving token to background script

--- PWMAN/Scripts/background.js
+++ PWMAN/Scripts/background.js
@@ -1,1 +1,16 @@
-console.log("we go here");
+let token = null;
+chrome.runtime.onMessage.addListener(
+    //Listens for messages sent
+    function(request, sender, sendResponse) {
+        //checks if token is set
+        if (request.token !== undefined) {
+            token = request.token;
+            console.log(request.token);
+            sendResponse({ success: true });
+
+            //check for request for token
+        } else if (request.getToken === true) {
+            console.log("sending token");
+            sendResponse({ token: token });
+        };
+    });
