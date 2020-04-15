083e8740316c441a76be054c20727bb34c4717ec
Jacob <jgje18@student.aau.dk>
2020-04-15 15:36:01 +0200

    added pepper and hash to password

--- PWMAN/Scripts/index.js
+++ PWMAN/Scripts/index.js
@@ -19,47 +19,66 @@ chrome.runtime.sendMessage({ getToken: true }, function(response) {
 async function formSubmit(event) {
     event.preventDefault();
 
-
-    chrome.tabs.query({ active: true }, async function(tabs) { //finder hvilken tab det hele bliver kaldt i. bør omfaktoreres 
-        let location = tabs[0].url;
         let form = document.getElementById("form"); //skaffer info fra form og indsætter i jsondata
-        let jsondata = {
-            username: form.username.value,
-            password: form.password.value,
-            domain: location
-        };
+        console.log(form.username.value);
+       
+        //Funktionen som henter den gemte peber streng fra local storage.
+        
+        /*let pepper = chrome.storage.local.get(['key'], function(result) {
+            let noget = JSON.stringify(result.key);
+            return noget;
+        });*/
+        chrome.storage.local.get(['key'], async function(result) {
+            
+            let form = document.getElementById("form"); //skaffer info fra form og indsætter i jsondata
+            let username = form.username.value;
+            console.log(username);
+            let pepperPass = form.password.value + result.key;
+            let hashedPass = hashing(pepperPass);
+            console.log("key: "+ result.key);
+            console.log("med peber: " + pepperPass);
+            console.log("hashed: " + hashedPass);
+            
+            let jsondata = {
+                username: form.username.value,
+                password: hashedPass,
+                domain: location
+            };
+            console.log(jsondata);
 
-        let answer = await fetch("http://127.0.0.1:3000/validate", { //kontakter serveren med username og password for at kunne logge ind.
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(jsondata, null, 2)
-        });
-
-        answer = await answer.json(); //parser responsen
-        console.log(answer.token); /*Skal fjernes på et tidspunkt*/
-
-
-        if (answer == "no user with given credentials") { //giver error message
-            /* TODO
-            STUB 
-            her indsættes error message om bruger ikke eksister eller forkert login oplysning
-            */
-
-        } else {
-
-            chrome.runtime.sendMessage({ token: "bearer " + answer.token }, function(response) {
-                //saves the token to backgroundscript
-                console.log("Bearer token successfully saved");
-                //TODOcheck if response.success==true. hvis den gør det. redirect. ellers error message.
-                //TODO redirect user.
+            let answer = await fetch("http://127.0.0.1:3000/validate", { //kontakter serveren med username og password for at kunne logge ind.
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(jsondata, null, 2)
             });
 
+            answer = await answer.json(); //parser responsen
+            console.log(answer); /*Skal fjernes på et tidspunkt*/
 
-        }
-        form.reset();
-    });
+
+            if (answer == "no user with given credentials") { //giver error message
+                /* TODO
+                STUB 
+                her indsættes error message om bruger ikke eksister eller forkert login oplysning
+                */
+
+            } else {
+
+                chrome.runtime.sendMessage({ token: "bearer " + answer.token }, function(response) {
+                    //saves the token to backgroundscript
+                    console.log("Bearer token successfully saved");
+                    //TODOcheck if response.success==true. hvis den gør det. redirect. ellers error message.
+                    //TODO redirect user.
+                });
+
+            }
+            form.reset();
+        })
+
+        
+        
 }
 
 //let newAnswer = await fetch("http://127.0.0.1:3000/test", { //sender test til server for at se om JWT er korrekt signed. bør fjernes at some point.
@@ -73,3 +92,13 @@ async function formSubmit(event) {
 
 //newAnswer = await newAnswer.json();
 //console.log(newAnswer);
+function hashing(str){ //stjålet fra nettet: http://mediocredeveloper.com/wp/?p=55
+    len = str.length;
+    hash = 0;
+    for(i=1; i<=len; i++){
+        char = str.charCodeAt((i-1));
+        hash += char*Math.pow(31,(len-i));
+        hash = hash & hash; //javascript limitation to force to 32 bits
+    }
+    return Math.abs(hash);
+}
