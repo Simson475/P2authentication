6bbdc93d033985380b4a7ab505c655e504db4fbc
Mathias Nielsen <mathias720@live.dk>
2020-04-20 15:19:04 +0200

    generate passsword funktion oprettet
    please look at me

--- PWMAN/Scripts/createPassword.js
+++ PWMAN/Scripts/createPassword.js
@@ -1,6 +1,7 @@
-document.querySelector("form").addEventListener("submit", generatePassword)
+document.querySelector("form").addEventListener("submit", addAccount)
 
-async function generatePassword(event){
+/* const cryptoRandomString = require('crypto-random-string'); */
+async function addAccount(event){
     event.preventDefault()
     let form = document.getElementById("form");
     console.log("username: " + form.username.value)
@@ -21,14 +22,14 @@ async function generatePassword(event){
                 let form = document.getElementById("form"); //skaffer info fra form og indsætter i jsondata
                 let activeTab = tabs[0];    
                 let location = activeTab.url;
-            
+                
                 console.log(activeTab.url);
 
                 let username2 = form.username.value;
                 console.log(username2);
                 let jsondata = {
                     username: form.username.value,
-                    password: form.password.value,
+                    password: generatePassword(),
                     domain: location
                 };
                 console.log(jsondata);
@@ -44,7 +45,7 @@ async function generatePassword(event){
                 answer = await answer.json()
                 console.log(answer);
                 if (answer == true){
-                    console.log("user added");
+                    console.log("Account added");
                     let message = document.getElementById("h3");
                     let Form = document.getElementById("form");
                     Form.style.display = "none";
@@ -60,18 +61,34 @@ async function generatePassword(event){
                 }
                 else{
                     let message = document.getElementById("h3");
-                    message.innerHTML = "Error - user not added";
+                    message.innerHTML = "Error - account not added";
                     message.style.display = "inline"; //viser error message
                     
-                    
-        
                 }
                 form.reset();
-            })
-            
-        }
-        
+            })   
+        }  
     })
-    
-    
 }
+
+function generatePassword() {
+    let password = [], str = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'N', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'];
+    for (let i = 0; i < 15; i++) {
+        password[i] = str[Math.floor(Math.random() * 61)];
+        console.log(password[i])
+    }
+    return password.join('');
+}
+
+function printsURL() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        // since only one tab should be active and in the current window at once
+        // the return variable should only have one entry
+        let activeTab = tabs[0];    
+        let location = activeTab.url;
+        location = location.split("");
+        location = location.slice(8, ).join('');
+        document.getElementById("URLgoesHere").innerHTML = location;
+    })
+}
+printsURL();
